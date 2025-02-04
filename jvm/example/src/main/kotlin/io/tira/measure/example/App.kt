7ad6629bd986399357804bba06365134cd9264e7
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-02-04 11:51:31 +0100

    Improve JVM API design

--- jvm/example/src/main/kotlin/io/tira/measure/example/App.kt
+++ jvm/example/src/main/kotlin/io/tira/measure/example/App.kt
@@ -1,16 +1,15 @@
-
 package io.tira.measure.example
 
 import io.tira.measure.*
 import java.lang.Thread.sleep
 
-fun logCallback(level: LogLevel, component: String, message: String) {
+private fun logCallback(level: LogLevel, component: String, message: String) {
     println("[${level.name}] [$component] $message")
 }
 
 fun main() {
-    println(dataProviders)
-    val result = measuring(setOf(MeasureType.TIME_ELAPSED_WALL_CLOCK), 100, ::logCallback) {
+    println(providers)
+    val result = measure(setOf(Measure.TIME_ELAPSED_WALL_CLOCK), 100, ::logCallback) {
         sleep(2000)
     }
     println(result)
--- jvm/library/src/main/kotlin/io/tira/measure/Measures.kt
+++ jvm/library/src/main/kotlin/io/tira/measure/Measures.kt
@@ -1,3 +1,5 @@
+@file:JvmName("Measures")
+
 package io.tira.measure
 
 import com.sun.jna.*
@@ -14,15 +16,15 @@ enum class LogLevel(val id: Int) {
 
     companion object {
         fun fromInt(id: Int): LogLevel {
-            if (entries.none { it.id == id }) {
-                throw IllegalArgumentException("LogLevel with id '$id' does not exist.")
+            if (entries.none { level -> level.id == id }) {
+                throw IllegalArgumentException("${LogLevel::class.simpleName} with id '$id' does not exist.")
             }
-            return entries.first { it.id == id }
+            return entries.first { level -> level.id == id }
         }
     }
 }
 
-enum class MeasureType(val id: String, val providerId: String) {
+enum class Measure(val id: String, val providerId: String) {
     TIME_ELAPSED_WALL_CLOCK("elapsed time.wallclock (ms)", "system") {
         override fun decode(pointer: Pointer): Any = pointer.getFloat(0)
     },
@@ -51,33 +53,47 @@ enum class MeasureType(val id: String, val providerId: String) {
     abstract fun decode(pointer: Pointer): Any
 
     companion object {
-        fun fromId(id: String): MeasureType {
-            if (entries.none { it.id == id }) {
-                throw IllegalArgumentException("MeasureType with id '$id' does not exist.")
+        fun fromId(id: String): Measure {
+            if (entries.none { measure -> measure.id == id }) {
+                throw IllegalArgumentException("${Measure::class.simpleName} with id '$id' does not exist.")
             }
-            return entries.first { it.id == id }
+            return entries.first { measure -> measure.id == id }
         }
     }
 }
 
-private interface LogCallback : Callback {
+private interface LoggingCallback : Callback {
     fun invoke(level: Int, component: String, message: String)
 }
 
-@Suppress("unused")
 @FieldOrder("name", "description", "version")
-class DataProvider : Structure() {
-    @JvmField var name: String = ""
-    @JvmField var description: String = ""
-    @JvmField var version: String = ""
+internal class DataProvider : Structure() {
+    @JvmField
+    var name: String = ""
+
+    @JvmField
+    var description: String = ""
+
+    @JvmField
+    var version: String? = ""
+
+    fun toProvider(): Provider {
+        return Provider(name, description, version)
+    }
 }
 
-@Suppress("unused")
+
+data class Provider(
+    val name: String,
+    val description: String,
+    val version: String?,
+)
+
 @FieldOrder("providers", "monitor", "pollIntervalMillis")
 internal class Config(
-    @JvmField var providers: Pointer,
-    @JvmField var monitor: Boolean,
-    @JvmField var pollIntervalMillis: Long,
+    @Suppress("unused") @JvmField var providers: Pointer,
+    @Suppress("unused") @JvmField var monitor: Boolean,
+    @Suppress("unused") @JvmField var pollIntervalMillis: Long,
 ) : Structure(), Structure.ByValue
 
 typealias MeasurementRef = Pointer
@@ -86,13 +102,16 @@ internal typealias MeasurementResultRef = Pointer
 
 @FieldOrder("name", "value")
 internal class ResultEntry : Structure() {
-    @JvmField var name: String = ""
-    @JvmField var value: MeasurementResultRef? = null
+    @JvmField
+    var name: String = ""
+
+    @JvmField
+    var value: MeasurementResultRef? = null
 }
 
 private interface MeasureLibrary : Library {
     fun mapiGetDataProviders(buffer: Array<DataProvider>?, bufferSize: Int): Int
-    fun mapiSetLogCallback(callback: LogCallback?)
+    fun mapiSetLogCallback(callback: LoggingCallback?)
     fun mapiStartMeasure(config: Config): MeasurementRef
     fun mapiStopMeasure(measure: MeasurementRef): MeasurementResultRef
     fun mapiResultGetValue(measurementResultRef: MeasurementResultRef, value: Pointer?): Boolean
@@ -105,34 +124,34 @@ private interface MeasureLibrary : Library {
     fun mapiResultFree(measurementResultRef: MeasurementResultRef)
 }
 
-private object CMeasureLibrary : MeasureLibrary by Native.load("measureapi", MeasureLibrary::class.java)
+private object NativeMeasureLibrary : MeasureLibrary by Native.load("measureapi", MeasureLibrary::class.java)
 
-val dataProviders: List<DataProvider>
+val providers: List<Provider>
     get() {
-        val numProviders = CMeasureLibrary.mapiGetDataProviders(null, 0)
+        val numProviders = NativeMeasureLibrary.mapiGetDataProviders(null, 0)
         if (numProviders == 0) {
             return emptyList()
         }
         @Suppress("UNCHECKED_CAST") val buffer = DataProvider().toArray(numProviders) as Array<DataProvider>
-        CMeasureLibrary.mapiGetDataProviders(buffer, numProviders)
-        return buffer.asList()
+        NativeMeasureLibrary.mapiGetDataProviders(buffer, numProviders)
+        return buffer.map { provider -> provider.toProvider() }
     }
 
 private fun Iterable<String>.toStringArray() = StringArray(this.toList().toTypedArray())
 
 fun startMeasurement(
-    measures: Iterable<MeasureType>,
+    measures: Iterable<Measure>,
     pollIntervalMillis: Long? = null,
     logCallback: ((level: LogLevel, component: String, message: String) -> Unit)? = null,
 ): MeasurementRef {
-    val internalLogCallback = if (logCallback != null) object : LogCallback {
+    val internalLogCallback = if (logCallback != null) object : LoggingCallback {
         override fun invoke(level: Int, component: String, message: String) {
             logCallback(LogLevel.fromInt(level), component, message)
         }
     } else null
-    CMeasureLibrary.mapiSetLogCallback(internalLogCallback)
+    NativeMeasureLibrary.mapiSetLogCallback(internalLogCallback)
 
-    val providers = measures.map { it.providerId }.toSet().toStringArray()
+    val providers = measures.map { measure -> measure.providerId }.toSet().toStringArray()
 
     val config = if (pollIntervalMillis != null) Config(
         providers = providers,
@@ -144,54 +163,54 @@ fun startMeasurement(
         pollIntervalMillis = 0L,
     )
 
-    val measurement = CMeasureLibrary.mapiStartMeasure(config)
+    val measurement = NativeMeasureLibrary.mapiStartMeasure(config)
     return measurement
 }
 
-private fun MeasurementResultRef.parse(name: String = ""): Map<MeasureType, Any> {
-    val isLeaf = CMeasureLibrary.mapiResultGetValue(this, null)
+private fun MeasurementResultRef.parse(name: String = ""): Map<Measure, Any> {
+    val isLeaf = NativeMeasureLibrary.mapiResultGetValue(this, null)
     if (isLeaf) {
-        val measureType = MeasureType.fromId(name)
+        val measure = Measure.fromId(name)
 
         val pointerPointer = Memory(Native.POINTER_SIZE.toLong())
-        CMeasureLibrary.mapiResultGetValue(this, pointerPointer)
+        NativeMeasureLibrary.mapiResultGetValue(this, pointerPointer)
         val valuePointer = pointerPointer.getPointer(0)
 
-//        val value = measureType.decode(valuePointer)
+        // TODO: Decode the value here, depending on the expected measure value type.
         val value = valuePointer.getString(0)
         pointerPointer.close()
 
-        return mapOf(measureType to value)
+        return mapOf(measure to value)
     } else {
         val namePrefix = if (name != "") "$name." else ""
-        val size = CMeasureLibrary.mapiResultGetEntries(this, null, 0)
+        val size = NativeMeasureLibrary.mapiResultGetEntries(this, null, 0)
         if (size == 0) {
             return emptyMap()
         }
 
         @Suppress("UNCHECKED_CAST") val entries = ResultEntry().toArray(size) as Array<ResultEntry>
-        CMeasureLibrary.mapiResultGetEntries(this, entries, size)
+        NativeMeasureLibrary.mapiResultGetEntries(this, entries, size)
 
-        return entries.flatMap {
-            it.value?.parse("$namePrefix${it.name}")?.toList() ?: listOf()
+        return entries.flatMap { entry ->
+            requireNotNull(entry.value).parse("$namePrefix${entry.name}").toList()
         }.toMap()
     }
 }
 
-fun stopMeasurement(measurement: MeasurementRef): Map<MeasureType, Any> {
-    val result = CMeasureLibrary.mapiStopMeasure(measurement)
-    CMeasureLibrary.mapiSetLogCallback(null)
+fun stopMeasurement(measurement: MeasurementRef): Map<Measure, Any> {
+    val result = NativeMeasureLibrary.mapiStopMeasure(measurement)
+    NativeMeasureLibrary.mapiSetLogCallback(null)
     val parsedResults = result.parse()
-    CMeasureLibrary.mapiResultFree(result)
+    NativeMeasureLibrary.mapiResultFree(result)
     return parsedResults
 }
 
-inline fun measuring(
-    measures: Iterable<MeasureType>,
+inline fun measure(
+    measures: Iterable<Measure>,
     pollIntervalMillis: Long? = null,
     noinline logCallback: ((level: LogLevel, component: String, message: String) -> Unit)? = null,
     crossinline block: () -> Unit,
-): Map<MeasureType, Any> {
+): Map<Measure, Any> {
     val measurement = startMeasurement(
         measures = measures,
         pollIntervalMillis = pollIntervalMillis,
@@ -206,3 +225,29 @@ inline fun measuring(
     return stopMeasurement(measurement)
 }
 
+interface JvmLogCallback {
+    fun invoke(level: LogLevel, component: String, message: String)
+}
+
+interface JvmBlockCallback {
+    fun invoke()
+}
+
+fun measure(
+    measures: Iterable<Measure>,
+    pollIntervalMillis: Long? = null,
+    logCallback: JvmLogCallback? = null,
+    block: JvmBlockCallback,
+): Map<Measure, Any> {
+    return measure(
+        measures,
+        pollIntervalMillis,
+        logCallback = if (logCallback != null) {
+            { level: LogLevel, component: String, message: String ->
+                logCallback.invoke(level, component, message)
+            }
+        } else null,
+        block = {
+            block.invoke()
+        })
+}
