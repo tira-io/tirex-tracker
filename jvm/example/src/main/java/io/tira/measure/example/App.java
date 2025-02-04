74f2f840a584822a54b5fa2d602a608825c04093
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-02-04 12:54:26 +0100

    Simplify JVM examples

--- jvm/example/src/main/java/io/tira/measure/example/App.java
+++ jvm/example/src/main/java/io/tira/measure/example/App.java
@@ -6,15 +6,13 @@ import io.tira.measure.*;
 import java.util.Map;
 import java.util.Set;
 
-import static io.tira.measure.Measures.getProviders;
-
 class App {
     private static void logCallback(LogLevel level, String component, String message) {
         System.out.printf("[%s] [%s] %s%n", level, component, message);
     }
 
     public static void main(String[] args) {
-        System.out.println(getProviders());
+        System.out.println(Measures.getProviders());
         Set<Measure> measures = Set.of(Measure.TIME_ELAPSED_WALL_CLOCK);
         Map<Measure, Object> result = Measures.measure(measures, 100L, App::logCallback, () -> {
             try {
