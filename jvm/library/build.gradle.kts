b59f6b3d1f87c47e6c4c0ad78f9e3510951f1f78
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-02-18 09:52:04 +0100

    Prepare Java Maven publish

--- jvm/library/build.gradle.kts
+++ jvm/library/build.gradle.kts
@@ -1,6 +1,10 @@
+import groovy.lang.Closure
+
 plugins {
-    id("org.gradle.java-library")
     kotlin("jvm") version "2.1.10"
+    `java-library`
+    `maven-publish`
+    id("com.palantir.git-version") version "3.1.0"
 }
 
 repositories {
@@ -13,11 +17,65 @@ dependencies {
     implementation("net.java.dev.jna:jna-platform:5.16.0")
     api("net.java.dev.jna:jna:5.16.0")
 
-
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 kotlin {
     jvmToolchain(8)
+}
+
+val gitVersion: Closure<String> by extra
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/tira-io/measure")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            groupId = "io.tira"
+            artifactId = "measure"
+            version = gitVersion()
+
+            from(components["java"])
+
+            pom {
+                name = "measure"
+                description = "Measuring what really matters."
+                url = "https://github.com/tira-io/measure"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/license/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        name = "Jan Heinrich Merker"
+                        email = "heinrich.merker@uni-jena.de"
+                    }
+                    developer {
+                        name = "Tim Hagen"
+                        email = "tim.hagen@uni-kassel.de"
+                    }
+                    developer {
+                        name = "Maik Fröbe"
+                        email = "maik.froebe@uni-jena.de"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/tira-io/measure.git"
+                    developerConnection = "scm:git:ssh://git@github.com:tira-io/measure.git"
+                    url = "https://github.com/tira-io/measure"
+                }
+            }
+        }
+    }
 }
