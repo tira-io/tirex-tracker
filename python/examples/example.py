5d8ef8945559313e86ca4d79d9528888038934ec
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-02-24 23:49:45 +0100

    Update example

--- python/examples/example.py
+++ python/examples/example.py
@@ -1,4 +1,10 @@
-from tirex_tracker import tracking
+from tirex_tracker import tracking, set_log_callback, LogLevel
+
+def log_callback(level: LogLevel, component: str, message: str) -> None:
+    print(f'[{level}] [{component}] {message}')
+
+set_log_callback(log_callback)
+
 with tracking() as results:
     from time import sleep
     sleep(1)
