eb6d4e977f1759332fd24983f257ff5a08a26710
Tim <timhagenmail@googlemail.com>
2025-03-03 21:03:19 +0100

    rewrote compatibility code

--- c/src/measure/utils/rangeutils.hpp
+++ c/src/measure/utils/rangeutils.hpp
@@ -5,17 +5,16 @@
 #include <sstream>
 #include <string>
 
-#if !__cpp_lib_format
+/** For compatibiltiy with clang-18: implement operator<< for std::chrono::duration **/
 #include <fmt/chrono.h>
 #include <fmt/core.h>
 
 template <typename _CharT, typename _Traits, typename _Rep, typename _Period>
 inline std::basic_ostream<_CharT, _Traits>&
-operator<<(std::basic_ostream<_CharT, _Traits>& os, const std::chrono::duration<_Rep, _Period>& d) {
+operator<<(std::basic_stringstream<_CharT, _Traits>& os, const std::chrono::duration<_Rep, _Period>& d) {
 	return os << fmt::format("{}", d);
 }
-
-#endif
+/** End of compatibility **/
 
 namespace tirex::utils {
 	inline std::string join(const std::ranges::range auto& range, char delimiter = ',') {
