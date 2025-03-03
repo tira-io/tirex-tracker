758c4a22c594da5960a98e2b918a6cce823213fe
Tim <timhagenmail@googlemail.com>
2025-03-03 18:37:44 +0100

    tried another fallback implementation

--- c/src/measure/utils/rangeutils.hpp
+++ c/src/measure/utils/rangeutils.hpp
@@ -5,27 +5,15 @@
 #include <sstream>
 #include <string>
 
-#if __cplusplus < 202002L
-/**
- * Implementation copied from chrono_io.h to support the operator on clang-18.
- */
-#include <chrono>
-namespace std {
-	template <typename _CharT, typename _Traits, typename _Rep, typename _Period>
-	inline basic_ostream<_CharT, _Traits>&
-	operator<<(std::basic_ostream<_CharT, _Traits>& __os, const duration<_Rep, _Period>& __d) {
-		using _Out = ostreambuf_iterator<_CharT, _Traits>;
-		using period = typename _Period::type;
-		std::basic_ostringstream<_CharT, _Traits> __s;
-		__s.flags(__os.flags());
-		__s.imbue(__os.getloc());
-		__s.precision(__os.precision());
-		__s << __d.count();
-		__detail::__fmt_units_suffix<period, _CharT>(_Out(__s));
-		__os << std::move(__s).str();
-		return __os;
-	}
-} // namespace std
+#if !__cpp_lib_format
+#include <fmt/core.h>
+
+template <typename _CharT, typename _Traits, typename _Rep, typename _Period>
+inline std::basic_ostream<_CharT, _Traits>&
+operator<<(std::basic_ostream<_CharT, _Traits>& os, const std::chrono::duration<_Rep, _Period>& d) {
+	return os << fmt::format("{}", d);
+}
+
 #endif
 
 namespace tirex::utils {
@@ -33,6 +21,7 @@ namespace tirex::utils {
 #ifdef __cpp_lib_ranges_join_with
 		return range | std::join_with(delimiter);
 #else
+
 		std::stringstream stream;
 		bool first = true;
 		for (auto& elem : range) {
