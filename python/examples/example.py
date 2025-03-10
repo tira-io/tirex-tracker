df6572f3d8328e4d8558ee44d2ab596eedd1766b
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-03-10 23:06:18 +0100

    Simplify examples

--- python/examples/example.py
+++ python/examples/example.py
@@ -1,5 +1,5 @@
 from tirex_tracker import tracking, ExportFormat
 
-with tracking(export_format = ExportFormat.IR_METADATA, export_file_path="irmetadata.yml") as results:
+with tracking(export_format = ExportFormat.IR_METADATA, export_file_path="ir_metadata.yml") as results:
     from time import sleep
     sleep(1)
