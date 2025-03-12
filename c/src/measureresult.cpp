fa7329b02786a994df1fa84c73e8f72e2a82bb9e
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-03-12 15:38:15 +0100

    Fix/simplify/backport time series serialization

--- c/src/measureresult.cpp
+++ c/src/measureresult.cpp
@@ -1,7 +1,6 @@
 #include <tirex_tracker.h>
 
 #include "measure/stats/provider.hpp"
-#include "measure/utils/rangeutils.hpp"
 
 #if __cpp_lib_format and __cplusplus >= 202207L // __cplusplus >= 202207L required for std::format_string
 #include <format>
@@ -12,6 +11,7 @@ namespace _fmt = fmt;
 #endif
 
 #include <iostream>
+#include <sstream>
 
 struct tirexResult_st {
 public:
@@ -38,21 +38,39 @@ tirexError tirexResultEntryNum(const tirexResult* result, size_t* num) {
 
 void tirexResultFree(tirexResult* result) { delete result; }
 
-template <class... Ts>
-struct overloaded : Ts... {
-	using Ts::operator()...;
-};
-
-template <class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
-
-template <typename T>
-static std::string toYAML(const tirex::TimeSeries<T>& timeseries) {
+static std::string toYAML(const tirex::TimeSeries<unsigned>& timeseries) {
 	const auto& [timestamps, values] = timeseries.timeseries();
+
+	std::stringstream timestampsStream;
+	timestampsStream << "[";
+	bool first = true;
+	for (auto& elem : timestamps) {
+		if (!first)
+			timestampsStream << ",";
+		timestampsStream << "\"";
+		timestampsStream << elem.count();
+		timestampsStream << "\"";
+		first = false;
+	}
+	timestampsStream << "]";
+
+	std::stringstream valuesStream;
+	valuesStream << "[";
+	bool first = true;
+	for (auto& elem : values) {
+		if (!first)
+			valuesStream << ",";
+		valuesStream << "\"";
+		valuesStream << elem;
+		valuesStream << "\"";
+		first = false;
+	}
+	valuesStream << "]";
+
 	return _fmt::format(
-			"{{max: {}, min: {}, avg: {}, timeseries: {{timestamps: [{}], values: [{}]}}}}", timeseries.maxValue(),
-			timeseries.minValue(), timeseries.avgValue(), tirex::utils::join(timestamps, ','),
-			tirex::utils::join(values, ',')
+			"{{\"max\":{},\"min\":{},\"avg\":{},\"timeseries\": {{\"timestamps\": [{}], \"values\": [{}]}}}}",
+			timeseries.maxValue(), timeseries.minValue(), timeseries.avgValue(), timestampsStream.str(),
+			valuesStream.str()
 	);
 }
 
