b95a4566643ab75472c208c809d5f07f63979998
Maik Fröbe <maik.froebe@uni-jena.de>
2025-05-07 15:14:23 +0200

    mf

--- python/tirex_tracker/archive_utils.py
+++ python/tirex_tracker/archive_utils.py
@@ -93,15 +93,14 @@ def create_git_zip_archive(
         return None
     working_tree_dir_path = Path(working_tree_dir)
 
-    tracked_paths = None
     try:
-        tracked_paths = repo.commit().tree.traverse()
+        repo_items = repo.commit().tree.traverse()
     except ValueError:
         return None
 
     tracked_paths = (
         Path(item.abspath)
-        for item in tracked_paths
+        for item in repo_items
         if isinstance(item, IndexObject)
     )
     tracked_paths = (
