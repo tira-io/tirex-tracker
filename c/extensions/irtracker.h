ab3bac605203bf322d2ff80da9fa205b2a7e2a40
Tim <timhagenmail@googlemail.com>
2025-08-06 18:44:47 +0200

    further cpp docs

--- c/extensions/irtracker.h
+++ c/extensions/irtracker.h
@@ -1,3 +1,8 @@
+/**
+ * @file irtracker.h
+ * @brief Contains the ir extension for tirex tracker
+ */
+
 #ifndef TIREX_TRACKEREXT_IRTRACKER_H
 #define TIREX_TRACKEREXT_IRTRACKER_H
 
@@ -6,10 +11,24 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+/**
+ * @defgroup irtracking Information Retrieval
+ * @brief
+ * @details
+ * @{
+ */
+/**
+ * @brief
+ * 
+ * @param info 
+ * @param result 
+ * @param filepath 
+ * @return TIREX_TRACKER_EXPORT 
+ */
 TIREX_TRACKER_EXPORT tirexError
 tirexResultExportIrMetadata(const tirexResult* info, const tirexResult* result, const char* filepath);
 
+/** @} */ // end of irtracking
 #ifdef __cplusplus
 }
 #endif
