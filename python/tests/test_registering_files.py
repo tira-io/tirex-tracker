778e9d7c7e9764942f85ac7f131b2ca6d91f912a
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-09-29 12:15:17 +0200

    Reformat, sort imports

--- python/tests/test_registering_files.py
+++ python/tests/test_registering_files.py
@@ -1,41 +1,43 @@
-import unittest
-from tirex_tracker import tracking, ExportFormat, register_file, clear_file_register
 import tempfile
-from pathlib import Path
+import unittest
 from glob import glob
+from pathlib import Path
+
+from tirex_tracker import ExportFormat, clear_file_register, register_file, tracking
+
 
 class TestRegisteredMetadata(unittest.TestCase):
-    def test_without_registered_value(self):
+    def test_without_registered_value(self) -> None:
         expected = ["metadata.yml"]
         clear_file_register()
 
         with tempfile.TemporaryDirectory() as tmp:
-            with tracking(export_file_path=Path(tmp)/"metadata.yml", export_format=ExportFormat.IR_METADATA):
+            with tracking(export_file_path=Path(tmp) / "metadata.yml", export_format=ExportFormat.IR_METADATA):
                 print("hello world")
-            actual = [i.split('/')[-1] for i in glob(f"{tmp}/*")]
+            actual = [i.split("/")[-1] for i in glob(f"{tmp}/*")]
 
         self.assertEqual(expected, actual)
 
-    def test_with_registered_value(self):
+    def test_with_registered_value(self) -> None:
         expected = set(["metadata.yml", "test_integration.py"])
         clear_file_register()
         register_file(Path(__file__).parent, Path("test_integration.py"))
 
         with tempfile.TemporaryDirectory() as tmp:
-            with tracking(export_file_path=Path(tmp)/"metadata.yml", export_format=ExportFormat.IR_METADATA):
+            with tracking(export_file_path=Path(tmp) / "metadata.yml", export_format=ExportFormat.IR_METADATA):
                 print("hello world")
-            actual = set([i.split('/')[-1] for i in glob(f"{tmp}/*")])
+            actual = set([i.split("/")[-1] for i in glob(f"{tmp}/*")])
 
         self.assertEqual(expected, actual)
 
-    def test_with_registered_value_to_subdir(self):
+    def test_with_registered_value_to_subdir(self) -> None:
         expected = set(["test_integration.py"])
         clear_file_register()
         register_file(Path(__file__).parent, Path("test_integration.py"), "subdir")
 
         with tempfile.TemporaryDirectory() as tmp:
-            with tracking(export_file_path=Path(tmp)/"metadata.yml", export_format=ExportFormat.IR_METADATA):
+            with tracking(export_file_path=Path(tmp) / "metadata.yml", export_format=ExportFormat.IR_METADATA):
                 print("hello world")
-            actual = set([i.split('/')[-1] for i in glob(f"{tmp}/subdir/*")])
+            actual = set([i.split("/")[-1] for i in glob(f"{tmp}/subdir/*")])
 
         self.assertEqual(expected, actual)
