fa794446d52abfaa355d3b058a13ecc4f3e58304
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-10-08 10:32:07 +0200

    Fix compat imports

--- python/tirex_tracker/_compat.py
+++ python/tirex_tracker/_compat.py
@@ -1,21 +1,19 @@
-from typing import TYPE_CHECKING
-
-from importlib_metadata import PackageNotFoundError, metadata
+from importlib_metadata import PackageNotFoundError, distribution
 from typing_extensions import TypeAlias  # type: ignore[import]
 
 
 def _is_installed(distribution_name: str) -> bool:
     try:
-        metadata(distribution_name=distribution_name)
+        distribution(distribution_name=distribution_name)
         return True
     except PackageNotFoundError:
         return False
 
 
 CODECARBON_INSTALLED = _is_installed("codecarbon")
-if CODECARBON_INSTALLED or TYPE_CHECKING:
-    from codecarbon import EmissionsTracker as EmissionsTracker  # type: ignore[redefine]
-    from codecarbon.emissions_tracker import BaseEmissionsTracker as BaseEmissionsTracker  # type: ignore[redefine]
+if CODECARBON_INSTALLED:
+    from codecarbon import EmissionsTracker as EmissionsTracker  # type: ignore
+    from codecarbon.emissions_tracker import BaseEmissionsTracker as BaseEmissionsTracker  # type: ignore
 else:
     EmissionsTracker: TypeAlias = NotImplemented  # type: ignore
     BaseEmissionsTracker: TypeAlias = NotImplemented  # type: ignore
