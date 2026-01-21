f705af9cf423ac9a28ba2d39e2aeef4d4897e7b7
Tim Hagen <timhagenmail@googlemail.com>
2026-01-21 08:16:35 +0000

    Report null for energy if not available

--- c/src/measure/stats/energystats.cpp
+++ c/src/measure/stats/energystats.cpp
@@ -2,17 +2,28 @@
 
 #include "../../logging.hpp"
 
+#include <optional>
+
 using namespace std::string_literals;
 
 using tirex::EnergyStats;
 using tirex::Stats;
 
-template <typename C, typename K, typename V>
-static V getOrDefault(const C& m, K const& key, const V& defval) {
+template <typename C, typename K>
+static auto tryget(const C& m, K const& key) -> std::optional<decltype(m.begin()->second)> {
 	auto it = m.find(key);
 	if (it == m.end())
-		return defval;
-	return it->second;
+		return std::nullopt;
+	return std::make_optional(it->second);
+}
+
+template <typename T, typename Fn>
+static auto transform(const std::optional<T>& opt, const Fn& transform)
+		-> std::optional<decltype(transform(opt.value()))> {
+	// Since we can't assume std::optional::transform from c++23 :(
+	if (opt.has_value())
+		return std::make_optional(transform(opt.value()));
+	return std::nullopt;
 }
 
 const char* EnergyStats::version = nullptr;
@@ -37,14 +48,16 @@ std::set<tirexMeasure> EnergyStats::providedMeasures() noexcept {
 void EnergyStats::start() { tracker.start(); }
 void EnergyStats::stop() { tracker.stop(); }
 Stats EnergyStats::getStats() {
+	using json = nlohmann::json;
 	auto results = tracker.calculate_energy().energy;
 	for (auto& [device, result] : results)
 		tirex::log::debug("cppjoules", "[{}] {}", device, result);
 	// Divide by 1000'000 since CPPJoules reports micro Joule (uJ)
+	auto divide = [](long long divisor) { return [divisor](long long divident) { return divident / divisor; }; };
 	return makeFilteredStats(
 			enabled,
-			std::pair{TIREX_CPU_ENERGY_SYSTEM_JOULES, std::to_string(getOrDefault(results, "core-0", 0) / 1000'000)},
-			std::pair{TIREX_RAM_ENERGY_SYSTEM_JOULES, std::to_string(getOrDefault(results, "dram-0", 0) / 1000'000)},
-			std::pair{TIREX_GPU_ENERGY_SYSTEM_JOULES, std::to_string(getOrDefault(results, "nvidia_gpu_0", 0) / 1000)}
+			std::pair{TIREX_CPU_ENERGY_SYSTEM_JOULES, json(transform(tryget(results, "core-0"), divide(1000'000)))},
+			std::pair{TIREX_RAM_ENERGY_SYSTEM_JOULES, json(transform(tryget(results, "dram-0"), divide(1000'000)))},
+			std::pair{TIREX_GPU_ENERGY_SYSTEM_JOULES, json(transform(tryget(results, "nvidia_gpu_0"), divide(1000)))}
 	);
 }
